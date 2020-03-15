943f7512cdd66fa508e0bf39478274f276e246e3
Shawn Craver <shawn@shauber.net>
2020-03-15 17:59:02 -0400

    script to import certs to Key Vault

--- Import-AcmeCertificateToKeyVault.ps1
+++ Import-AcmeCertificateToKeyVault.ps1
@@ -3,18 +3,62 @@ param (
     [string] $KeyVaultResourceId
 )
 
+if ($PSPrivateMetadata.JobId) {
+    $connectionName = "AzureRunAsConnection"
+    try
+    {
+        # Get the connection "AzureRunAsConnection "
+        $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+    
+        "Logging in to Azure..."
+        Add-AzAccount `
+            -ServicePrincipal `
+            -TenantId $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+    }
+    catch {
+        if (!$servicePrincipalConnection)
+        {
+            $ErrorMessage = "Connection $connectionName not found."
+            throw $ErrorMessage
+        } else{
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
+    }
+}
+
+$AcmeDirectory = "LE_STAGE" # "LE_PROD"
+
 # Split certificate names by comma or semi-colon
 $certificateName = $CertificateNames.Replace(',', ';') -split ';' | ForEach-Object -Process { $_.Trim() } | Select-Object -First 1
 
-# For wildcard certificates, Posh-ACME replaces * with ! in the directory name
+#For wildcard certificates, Posh-ACME replaces * with ! in the directory name
 $certificateName = $certificateName.Replace('*', '!')
 
-# Set working directory
-$workingDirectory = Join-Path -Path "." -ChildPath "pa"
+#make sure we have a Posh-ACME working directory
+if ([string]::IsNullOrWhiteSpace($env:POSHACME_HOME)) {
+	exit 1
+}
 
-# Set Posh-ACME working directory
-$env:POSHACME_HOME = $workingDirectory
-Import-Module -Name Posh-ACME -Force
+$workingDirectory = $env:POSHACME_HOME
+
+Import-Module Posh-ACME -Force
+
+#Configure Posh-ACME server
+Set-PAServer -DirectoryUrl $AcmeDirectory
+
+#Configure Posh-ACME account
+$account = Get-PAAccount
+if (-not $account) {
+    # New account
+    exit 1
+}
+elseif ($account.contact -ne "mailto:$AcmeContact") {
+    # Update account contact
+    exit 1
+}
 
 # Resolve the details of the certificate
 $currentServerName = ((Get-PAServer).location) -split "/" | Where-Object -FilterScript { $_ } | Select-Object -Skip 1 -First 1
@@ -27,20 +71,23 @@ $pfxFilePath = Join-Path -Path $orderDirectoryPath -ChildPath "fullchain.pfx"
 
 # If we have a order and certificate available
 if ((Test-Path -Path $orderDirectoryPath) -and (Test-Path -Path $orderDataPath) -and (Test-Path -Path $pfxFilePath)) {
-
     # Load order data
     $orderData = Get-Content -Path $orderDataPath -Raw | ConvertFrom-Json
 
-    # Load PFX
-    $certificate = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList $pfxFilePath, $orderData.PfxPass, 'EphemeralKeySet'
-
+    $pfxFilePath
+    $orderData.PfxPass
+    
     # Get the current certificate from key vault (if any)
     $azureKeyVaultCertificateName = $certificateName.Replace(".", "-").Replace("!", "wildcard")
     $keyVaultResource = Get-AzResource -ResourceId $KeyVaultResourceId
-    $azureKeyVaultCertificate = Get-AzKeyVaultCertificate -VaultName $keyVaultResource.Name -Name $azureKeyVaultCertificateName -ErrorAction SilentlyContinue
 
     # If we have a different certificate, import it
-    If (-not $azureKeyVaultCertificate -or $azureKeyVaultCertificate.Thumbprint -ne $certificate.Thumbprint) {
-        Import-AzKeyVaultCertificate -VaultName $keyVaultResource.Name -Name $azureKeyVaultCertificateName -FilePath $pfxFilePath -Password (ConvertTo-SecureString -String $orderData.PfxPass -AsPlainText -Force) | Out-Null
-    }
+    Import-AzKeyVaultCertificate -VaultName $keyVaultResource.Name `
+    				 -Name $azureKeyVaultCertificateName `
+				 -FilePath $pfxFilePath `
+				 -Password (ConvertTo-SecureString -String $orderData.PfxPass -AsPlainText -Force) | Out-Null
 }
+
+
+
+
