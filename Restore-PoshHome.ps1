d3a753ca4019691fd06ff5ed5dddff172934e011
Shawn Craver <shawn@shauber.net>
2020-03-15 17:06:10 -0400

    added script to pull POSHACME_HOME from a blob

---
+++ Restore-PoshHome.ps1
@@ -0,0 +1,60 @@
+param (
+    [string] $ResourceGroupName,
+    [string] $StorageAccountName
+)
+
+Write-Output $ResourceGroupName
+Write-Output $StorageAccountName
+
+if ($PSPrivateMetadata.JobId) {
+    $connectionName = "AzureRunAsConnection"
+    try
+    {
+        # Get the connection "AzureRunAsConnection "
+        $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+    
+        "Logging in to Azure..."
+        Add-AzAccount `
+            -ServicePrincipal `
+            -TenantId $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+    }
+    catch {
+        if (!$servicePrincipalConnection)
+        {
+            $ErrorMessage = "Connection $connectionName not found."
+            throw $ErrorMessage
+        } else{
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
+    }
+}
+
+
+#setup our storage context and related vars
+$storageKey = (Get-AzStorageAccountKey -ResourceGroupName "$ResourceGroupName" -AccountName "$StorageAccountName")[0].Value
+$StorageContext = New-AzStorageContext -StorageAccountName "$StorageAccountName" -StorageAccountKey $storageKey
+$PoshZipFile = "posh-acme.zip"
+$PoshZipPath = "$($env:TEMP)/$PoshZipFile"
+$zipContainer = "automation"
+
+#Create working directory
+$workingDirectory = Join-Path -Path "." -ChildPath "pa"
+New-Item -Path $workingDirectory -ItemType Directory | Out-Null
+
+#Sync contents of storage container to working directory
+$blob = Get-AzStorageBlob -Blob $PoshZipFile -Container $zipContainer -Context $StorageContext -ErrorAction Ignore
+if ($blob) {
+    Write-Output "We Have a blob, quick, extract it!"
+    Get-AzStorageBlobContent -Blob $PoshZipFile -Container $zipContainer -Destination $PoshZipPath -Context $StorageContext
+    Expand-Archive -Path $PoshZipPath -DestinationPath ".\" -Force
+    Get-ChildItem -Recurse $env:POSHACME_HOME
+} else {
+    Write-Output "NO BLOB HERE!"
+}
+
+#Set Posh-ACME working directory
+$env:POSHACME_HOME = $workingDirectory
+
