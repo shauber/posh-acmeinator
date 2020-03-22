f925125291df82a843900f109eb1eea9cf10c22f
Shawn Craver <shawn@shauber.net>
2020-03-22 16:12:54 -0400

    added automation account creation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 This repository was inspired from this article: 
 https://medium.com/@brentrobinson5/automating-certificate-management-with-azure-and-lets-encrypt-fee6729e2b78
+
+
+Must manually add an Azure Run as Account to the automation account after creation (not supported in Terraform)
+https://docs.microsoft.com/en-us/azure/automation/manage-runas-account#creating-a-run-as-account-in-azure-portal
--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "~> 2.2.0"
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
 }
 
 data "azurerm_resource_group" "automation-rg" {
@@ -10,12 +14,17 @@ data "azurerm_resource_group" "automation-rg" {
 
 data "azurerm_storage_account" "storage" {
   resource_group_name = data.azurerm_resource_group.automation-rg.name
-  name                = "automationstorage78"
+  name                     = "automationstorage78"
+  # location                 = azurerm_resource_group.automation-rg.location
+  # account_tier             = "Standard"
+  # account_kind             = "StandardV2"
+  # account_replication_type = "LRS"
 }
 
 data "azurerm_storage_container" "container" {
     storage_account_name = data.azurerm_storage_account.storage.name
     name                 = "automation"
+    # container_access_type = "private"
 }
 
 data "azurerm_key_vault" "kv" {
@@ -23,62 +32,64 @@ data "azurerm_key_vault" "kv" {
   name                = "spc-automation-kv"
 }
 
-data "azurerm_automation_account" "acmeinator" {
+resource "azurerm_automation_account" "cron" {
   resource_group_name = data.azurerm_resource_group.automation-rg.name
-  name                = "acmeinator"
+  location            = data.azurerm_resource_group.automation-rg.location
+  sku_name            = "Basic"
+  name                = "marvin"
 }
 
 resource "azurerm_automation_variable_string" "AcmeDirectory" {
   name                    = "AcmeDirectory"
   resource_group_name     = data.azurerm_resource_group.automation-rg.name
-  automation_account_name = data.azurerm_automation_account.acmeinator.name
+  automation_account_name = azurerm_automation_account.cron.name
   value                   = "LE_STAGE"
 }
 
 resource "azurerm_automation_variable_string" "RGVar" {
   name                    = "ResourceGroupName"
   resource_group_name     = data.azurerm_resource_group.automation-rg.name
-  automation_account_name = data.azurerm_automation_account.acmeinator.name
+  automation_account_name = azurerm_automation_account.cron.name
   value                   = data.azurerm_resource_group.automation-rg.name
 }
 
 resource "azurerm_automation_variable_string" "SAVar" {
   name                    = "StorageAccountName"
   resource_group_name     = data.azurerm_resource_group.automation-rg.name
-  automation_account_name = data.azurerm_automation_account.acmeinator.name
+  automation_account_name = azurerm_automation_account.cron.name
   value                   = data.azurerm_storage_account.storage.name
 }
 
 resource "azurerm_automation_variable_string" "KVVar" {
   name                    = "KeyVaultResourceId"
   resource_group_name     = data.azurerm_resource_group.automation-rg.name
-  automation_account_name = data.azurerm_automation_account.acmeinator.name
+  automation_account_name = azurerm_automation_account.cron.name
   value                   = data.azurerm_key_vault.kv.id
 }
 
 resource "azurerm_automation_variable_string" "ContainerVar" {
   name                    = "StorageContainerName"
   resource_group_name     = data.azurerm_resource_group.automation-rg.name
-  automation_account_name = data.azurerm_automation_account.acmeinator.name
+  automation_account_name = azurerm_automation_account.cron.name
   value                   = data.azurerm_storage_container.container.name
 }
 
 resource "azurerm_automation_variable_string" "PoshZipVar" {
   name                    = "PoshZipName"
   resource_group_name     = data.azurerm_resource_group.automation-rg.name
-  automation_account_name = data.azurerm_automation_account.acmeinator.name
+  automation_account_name = azurerm_automation_account.cron.name
   value                   = "posh-acme.zip"
 }
 
 data "local_file" "ImportCert" {
-  filename = "${path.module}/Import-AcmeCertificateToKeyValut.ps1"
+  filename = "${path.module}/Import-AcmeCertificateToKeyVault.ps1"
 }
 
-resource "azurerm_automation_runbook" "NewCert" {
+resource "azurerm_automation_runbook" "ImportCert" {
   name                    = "Import-AcmeCertificateToKeyValut"
   location                = data.azurerm_resource_group.automation-rg.location
   resource_group_name     = data.azurerm_resource_group.automation-rg.name
-  automation_account_name = data.azurerm_automation_account.acmeinator.name
+  automation_account_name = azurerm_automation_account.cron.name
   log_verbose             = "true"
   log_progress            = "true"
   runbook_type            = "PowerShell"
@@ -98,7 +109,7 @@ resource "azurerm_automation_runbook" "NewCert" {
   name                    = "New-AcmeCertificate"
   location                = data.azurerm_resource_group.automation-rg.location
   resource_group_name     = data.azurerm_resource_group.automation-rg.name
-  automation_account_name = data.azurerm_automation_account.acmeinator.name
+  automation_account_name = azurerm_automation_account.cron.name
   log_verbose             = "true"
   log_progress            = "true"
   runbook_type            = "PowerShell"
@@ -118,7 +129,7 @@ resource "azurerm_automation_runbook" "RenewCerts" {
   name                    = "Renew-AcmeCertificates"
   location                = data.azurerm_resource_group.automation-rg.location
   resource_group_name     = data.azurerm_resource_group.automation-rg.name
-  automation_account_name = data.azurerm_automation_account.acmeinator.name
+  automation_account_name = azurerm_automation_account.cron.name
   log_verbose             = "true"
   log_progress            = "true"
   runbook_type            = "PowerShell"
@@ -138,7 +149,7 @@ resource "azurerm_automation_runbook" "RestorePosh" {
   name                    = "Restore-PoshHome"
   location                = data.azurerm_resource_group.automation-rg.location
   resource_group_name     = data.azurerm_resource_group.automation-rg.name
-  automation_account_name = data.azurerm_automation_account.acmeinator.name
+  automation_account_name = azurerm_automation_account.cron.name
   log_verbose             = "true"
   log_progress            = "true"
   runbook_type            = "PowerShell"
@@ -158,7 +169,7 @@ resource "azurerm_automation_runbook" "SavePosh" {
   name                    = "Save-PoshHome"
   location                = data.azurerm_resource_group.automation-rg.location
   resource_group_name     = data.azurerm_resource_group.automation-rg.name
-  automation_account_name = data.azurerm_automation_account.acmeinator.name
+  automation_account_name = azurerm_automation_account.cron.name
   log_verbose             = "true"
   log_progress            = "true"
   runbook_type            = "PowerShell"
